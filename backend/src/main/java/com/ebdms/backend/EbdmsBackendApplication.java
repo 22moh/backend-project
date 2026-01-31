76fb6a11b4978fd036cbe9260aba6120dfefcc94
mohummad Hisham <mohamadhishamhabeb@gmail.com>
2026-01-31 16:00:01 +0200

    First commit: Start backend project

---
+++ backend/src/main/java/com/ebdms/backend/EbdmsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ebdms.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EbdmsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EbdmsBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/ebdms/backend/EbdmsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ebdms.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EbdmsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
